761d74c523b5ed7693eec119ecfaf2f59f2b808a
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-01-13 11:24:28 +0530

    Add User Entity Class

---
+++ src/main/java/model/entity/User.java
@@ -0,0 +1,16 @@
+package model.entity;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class User {
+
+    private String firstName;
+    private String lastName;
+    private String password;
+
+}
