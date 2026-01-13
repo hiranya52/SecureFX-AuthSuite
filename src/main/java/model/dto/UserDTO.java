58e86bfdc37bfad6f3cd6ee96e59fa00ef0889b5
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-01-13 11:23:32 +0530

    Add User DTO Class

--- src/main/java/model/dto/UserDTO.java
+++ src/main/java/model/dto/UserDTO.java
@@ -2,6 +2,15 @@ package model.dto;
 
 import lombok.*;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class UserDTO {
+
+    private String firstName;
+    private String lastName;
+    private String password;
 
 }
