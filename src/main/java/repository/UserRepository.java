3d850d87ee19b0699876caf609c3d04070225749
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-01-13 14:04:35 +0530

    Add DBConnection class

--- src/main/java/repository/UserRepository.java
+++ src/main/java/repository/UserRepository.java
@@ -1,4 +1,12 @@
 package repository;
 
+import model.entity.User;
+
 public class UserRepository {
+
+    public void addUser(User user) {
+
+    }
+
+
 }
--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -1,11 +1,26 @@
 package service;
 
 import model.dto.UserDTO;
+import model.entity.User;
+import password.PasswordUtil;
+import repository.UserRepository;
 
 public class UserService {
 
+    UserRepository userRepository = new UserRepository();
+
     public void addUser(UserDTO userDTO) {
 
+        String hashedPassword = PasswordUtil.hashPassword(userDTO.getPassword());
+
+        User user = new User(
+                userDTO.getFirstName(),
+                userDTO.getLastName(),
+                userDTO.getEmail(),
+                hashedPassword
+        );
+
+        userRepository.addUser(user);
 
 
     }
